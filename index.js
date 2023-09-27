4d71d9c8e49c577b87a08cff1b8e98a17ede1433
tomitrecco <treccotomi@gmail.com>
2023-09-27 18:15:45 -0300

    correcciones pre entrega 2

--- index.js
+++ index.js
@@ -4,36 +4,37 @@ let apellidos=[];
 let nombre=0;
 let apellido=0;
 
-function crearpersona (nombre,apellido){
+function Crearpersona (nombre,apellido){
     this.nombre=nombre;
     this.apellido=apellido;
-
-    nombres.push(nombre)
-    apellidos.push(apellido)
+    this.agregarlista=function(){
+            nombres.push(nombre);
+            apellidos.push(apellido);
+    }    
        
 }
 
 
-function centraldeturnos(){
+function Centraldeturnos(){
 
-    let paciente=0
-    let contador=0
-    let contadorpaciente=0
+    let paciente=0;
+    let contador=0;
+    let contadorpaciente=0;
     while (contador<10){
         
-        paciente=parseInt(prompt("si tiene turno ingrese 1,si no tiene ingrese 0 para registrarse"))
+        paciente=parseInt(prompt("si tiene turno ingrese 1,si no tiene ingrese 0 para registrarse,si desea salir ingrese otro valor"));
         console.log(paciente);   
         if (paciente==1){
-            let buscador=prompt("ingrese su apellido").toUpperCase()
+            let buscador=prompt("ingrese su apellido").toUpperCase();
                 if(apellidos.includes(buscador)){
-                    contador=contador+1
-            contadorpaciente=contador   
-            console.log(contador);
-            alert ("usted es el paciente numero "+contadorpaciente+",la espera es de "+contador*15+" minutos" )
-            }
-            else{
-                alert("Porfavor registrese para sacar su turno")
-            }
+                    contador=contador+1;
+                    contadorpaciente=contador;   
+                    console.log(contador);
+                    alert ("usted es el paciente numero "+contadorpaciente+",la espera es de "+contador*15+" minutos" );
+                }
+                else{
+                    alert("Porfavor registrese para sacar su turno");
+                }
              
             
         }
@@ -42,12 +43,17 @@ function centraldeturnos(){
         else if (paciente==0){
             nombre=prompt("Ingrese su nombre para registrarse").toUpperCase();
             apellido=prompt("Ingrese su apellido para registrarse").toUpperCase();        
-            const persona=new crearpersona(nombre,apellido)                     
-            console.log(persona)
+            const persona=new Crearpersona(nombre,apellido);
+            persona.agregarlista()
+            contador=contador+1;
+            contadorpaciente=contador;  
+            alert ("usted es el paciente numero "+contadorpaciente+",la espera es de "+contador*15+" minutos" ); 
+                     
+
 
         }
         else if (paciente!=1 && paciente!=0){
-            alert("Ingrese un valor valido")
+            contador=11
         }
         
         
@@ -57,7 +63,7 @@ function centraldeturnos(){
 
 
 
-centraldeturnos()
+Centraldeturnos()
 
 
 
