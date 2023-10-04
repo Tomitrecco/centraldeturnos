fa513a426e5401cdaf45c5a01eaad9c58aa4a2d3
tomitrecco <treccotomi@gmail.com>
2023-10-04 20:04:06 -0300

    preentrega3

--- index.js
+++ index.js
@@ -1,69 +1,70 @@
-
-let nombres=[];
-let apellidos=[];
-let nombre=0;
-let apellido=0;
-
-function Crearpersona (nombre,apellido){
-    this.nombre=nombre;
-    this.apellido=apellido;
-    this.agregarlista=function(){
-            nombres.push(nombre);
-            apellidos.push(apellido);
-    }    
-       
+function Mostrarformularioregistro() {
+    var formulario = document.getElementById("formulario");
+    formulario.style.display = "block";
 }
 
+function Verificarturno(){ 
+    
+    document.getElementById("dniform").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    var dniinput = document.getElementById("dniinput").value;
+    var turnosRegistrados = JSON.parse(sessionStorage.getItem("turnosRegistrados")) || [];
+    var encontrado = false;
 
-function Centraldeturnos(){
-
-    let paciente=0;
-    let contador=0;
-    let contadorpaciente=0;
-    while (contador<10){
-        
-        paciente=parseInt(prompt("si tiene turno ingrese 1,si no tiene ingrese 0 para registrarse,si desea salir ingrese otro valor"));
-        console.log(paciente);   
-        if (paciente==1){
-            let buscador=prompt("ingrese su apellido").toUpperCase();
-                if(apellidos.includes(buscador)){
-                    contador=contador+1;
-                    contadorpaciente=contador;   
-                    console.log(contador);
-                    alert ("usted es el paciente numero "+contadorpaciente+",la espera es de "+contador*15+" minutos" );
-                }
-                else{
-                    alert("Porfavor registrese para sacar su turno");
-                }
-             
-            
+    for (var i = 0; i < turnosRegistrados.length; i++) {
+        if (turnosRegistrados[i].dni === dniinput) {
+            encontrado = true;
+            break;
         }
-           
-        
-        else if (paciente==0){
-            nombre=prompt("Ingrese su nombre para registrarse").toUpperCase();
-            apellido=prompt("Ingrese su apellido para registrarse").toUpperCase();        
-            const persona=new Crearpersona(nombre,apellido);
-            persona.agregarlista()
-            contador=contador+1;
-            contadorpaciente=contador;  
-            alert ("usted es el paciente numero "+contadorpaciente+",la espera es de "+contador*15+" minutos" ); 
-                     
-
-
-        }
-        else if (paciente!=1 && paciente!=0){
-            contador=11
-        }
-        
-        
     }
+
+    var mensaje = document.getElementById("mensaje");
+    if (encontrado) {
+        mensaje.textContent = "Tiene un turno programado.";
+        document.getElementById("dniinput").value = "";
+    } else {
+        mensaje.textContent = "No tiene un turno programado.";
+        Mostrarformularioregistro();
+        document.getElementById("dniinput").value = "";
+    }
+});
+
+    
 }
 
 
 
+function Registroturno(){
+    document.getElementById("registroformulario").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-Centraldeturnos()
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var dni = document.getElementById("dni").value;
 
+    var turno = {
+        nombre: nombre,
+        apellido: apellido,
+        dni: dni
+    };
 
+    var turnosRegistrados = JSON.parse(sessionStorage.getItem("turnosRegistrados")) || [];
+    turnosRegistrados.push(turno);
 
+    sessionStorage.setItem("turnosRegistrados", JSON.stringify(turnosRegistrados));
+
+    var mensaje = document.getElementById("mensaje");
+    mensaje.textContent = "Registro exitoso. Tiene un turno programado.";
+
+    var formulario = document.getElementById("formulario");
+    formulario.style.display = "none";
+
+    document.getElementById("nombre").value = "";
+    document.getElementById("apellido").value = "";
+    document.getElementById("dni").value = "";
+});
+}
+
+Verificarturno()
+Registroturno()
